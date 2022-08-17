3c76176b8d732905d6aeb4cf1f7879852e3ee58d
Marion L <mlelaumier@gmail.com>
2022-08-17 11:56:39 +0100

    added login function

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -63,27 +63,37 @@ exports.signup = (req, res) => {
   // }
 };
 
-// exports.login = (req, res, next) => {
-//   User.findOne({ email: req.body.email })
-//     .then((user) => {
-//       if (!user) {
-//         return res.status(401).json({ message: "Utilisateur inexistant" });
-//       }
-//       bcrypt
-//         .compare(req.body.password, user.password)
-//         .then((valid) => {
-//           if (!valid) {
-//             return res.status(401).json({ message: "Mot de passe incorrect" });
-//           }
-//           res.status(200).json({
-//             userId: user._id,
-//             token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-//               expiresIn: "1h",
-//             }),
-//           });
-//         })
-//         .catch((error) => res.status(500).json({ error }));
-//     })
-
-//     .catch((error) => res.status(500).json({ error }));
-// };
+exports.login = (req, res) => {
+  //finding email in database
+  const email = req.body.email;
+  db.query("SELECT * FROM user WHERE email=?", email, (error, results) => {
+    if (error) {
+      res.json({ error });
+    } else if (results == 0) {
+      return res.status(404).json({ error: "Utilisateur non trouve" });
+    } else {
+      //comparing password to allow user connection
+      const password = results[0].password;
+      bcrypt
+        .compare(req.body.password, password)
+        .then((valid) => {
+          if (!valid) {
+            res.status(401).json({ message: "mot de passe incorrect" });
+          } else {
+            //issuing a token to allow access throughout website
+            res.status(200).json({
+              userId: results[0].id,
+              token: jwt.sign(
+                { userId: results[0].id },
+                process.env.KEY_TOKEN,
+                {
+                  expiresIn: "1h",
+                }
+              ),
+            });
+          }
+        })
+        .catch((error) => res.status(500).json({ error }));
+    }
+  });
+};
