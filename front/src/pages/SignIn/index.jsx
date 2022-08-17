b2a4b001a8c9834b2ee7a0acbf601cc5302c0d9a
Marion L <mlelaumier@gmail.com>
2022-08-17 12:00:12 +0100

    login working and redirect ok

--- front/src/pages/SignIn/index.jsx
+++ front/src/pages/SignIn/index.jsx
@@ -1,13 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useRef } from "react";
 export default function SignIn() {
+  const passwordRef = useRef();
+  const emailRef = useRef();
+  let navigate = useNavigate();
+  // const [errorEmail, setErrorEmail] = useState(null);
+  function handleSubmit(event) {
+    event.preventDefault();
+    const savedEmail = emailRef.current.value;
+    const savedPassword = passwordRef.current.value;
+    const formData = {
+      email: savedEmail,
+      password: savedPassword,
+    };
+    const urlSignIn = "http://localhost:3001/api/auth/login";
+    fetch(urlSignIn, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify(formData),
+    }).then(function(res) {
+      if (res.ok) {
+        return res.json();
+      }
+    });
+
+    //redirect to feed page
+    navigate("/profile", { replace: true });
+  }
   return (
     <section className="signIn">
       <h1>Connectez-vous</h1>
       <form className="signIn__form">
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Mot de passe" />
-        <button className="signIn--button">Se connecter</button>
+        <input type="email" placeholder="Email" ref={emailRef} />
+        <input type="password" placeholder="Mot de passe" ref={passwordRef} />
+        <button className="signIn--button" onClick={handleSubmit}>
+          Se connecter
+        </button>
       </form>
       <p>Pas encore de compte?</p>
       <Link to="/signUp" className="signIn--button">
