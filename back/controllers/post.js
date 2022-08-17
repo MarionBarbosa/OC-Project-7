5d2e39c7ef9bad0dc3ed436d6af72a74aa592b91
Marion L <mlelaumier@gmail.com>
2022-08-17 11:58:23 +0100

    created routes

--- back/controllers/post.js
+++ back/controllers/post.js
@@ -1,4 +1,94 @@
-// const Post = require("../models/post");
+const Post = require("../models/post");
+const db = require("../mysql_config");
+exports.createPost = (req, res, next) => {
+  const postObject = JSON.parse(req.body.post);
+  //sending new post to database
+  //checking userId is the same to authorise new post
+  if (postObject.userId != req.auth.userId) {
+    res.status(401).json({ message: "Accès non autorisé" });
+  } else {
+    const post = new Post({
+      ...postObject,
+      imageUrl: `${req.protocol}://${req.get("host")}/images/${
+        req.file.filename
+      }`,
+    });
+    db.query(
+      "INSERT INTO post SET ?",
+      [post.title, post.content, post.userId, post.imageUrl],
+      (error, results) => {
+        if (error) {
+          res.json({ error });
+        } else if (results == 0) {
+          return res.status(404).json({ error: "Post non cree" });
+        } else {
+          res.status(201).json({ message: "post enregistre" });
+        }
+      }
+    );
+  }
+};
+
+exports.getAllPosts = (req, res, next) => {
+  db.query("SELECT * FROM post", (error, results) => {
+    if (error) {
+      res.json({ error });
+    } else if (results == 0) {
+      return res.status(404).json({ error: "Post non trouve" });
+    } else {
+      console.log(results);
+      res.status(200).json({ results });
+    }
+  });
+};
+exports.modifyPost = (req, res, next) => {
+  const newTitle = req.body.title;
+  const newContent = req.body.content;
+  const postId = req.body.id;
+
+  const post = new Post(newTitle, newContent);
+  db.query("SELECT * FROM post WHERE id=?", postId, (error, results) => {
+    if (error) {
+      res.json({ error });
+    } else if (results == 0) {
+      return res.status(404).json({ error: "Post inexistant" });
+    } else {
+      db.query(
+        `UPDATE post SET ? WHERE id=?`,
+        [post, postId],
+        (error, results) => {
+          if (error) {
+            res.json({ error });
+          } else if (results == 0) {
+            return res.status(404).json({ error: "Post non modifie" });
+          } else {
+            res.status(200).json({ message: "post modifie" });
+          }
+        }
+      );
+    }
+  });
+};
+exports.deletePost = (req, res, next) => {
+  const postId = req.body.id;
+  db.query("SELECT * FROM post WHERE id=?", postId, (error, results) => {
+    if (error) {
+      res.json({ error });
+    } else if (results == 0) {
+      return res.status(404).json({ error: "Post inexistant" });
+    } else {
+      db.query("DELETE FROM post WHERE id =?", postId, (error, results) => {
+        if (error) {
+          res.json({ error });
+        } else if (results == 0) {
+          return res.status(404).json({ error: "Post non supprime" });
+        } else {
+          res.status(200).json({ message: "post supprime " });
+        }
+      });
+    }
+  });
+};
 // const fs = require("fs");
 
 // exports.createPost = (req, res, next) => {
