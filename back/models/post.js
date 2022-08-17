25c5736766df4df37359bffeba260ffbb9c6193c
Marion L <mlelaumier@gmail.com>
2022-08-17 11:53:55 +0100

    created class to create post

--- back/models/post.js
+++ back/models/post.js
@@ -1,11 +1,19 @@
 /*
- *photo utilisateur
- *nom utilisateur
+ *photo utilisateur ==> JOIN from table user
+ *nom utilisateur==> JOIN from table user
  *date de publication
  *Titre du post/ optionnel
  *text du post
  *image du post/ opionnel
- *like
- *usersliked/usersDisliked
- *commentaire
+ *like==> JOIN from table liked
+ *commentaire ==> JOIN from table comment
  */
+class Post {
+  constructor(title, content, userId, imageUrl) {
+    this.title = title;
+    this.content = content;
+    this.userId = userId;
+    this.imageUrl = imageUrl;
+  }
+}
+module.exports = Post;
