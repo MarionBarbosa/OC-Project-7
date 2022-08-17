77797051c2250e410525de8f38ae0bcb991af026
Marion L <mlelaumier@gmail.com>
2022-08-17 11:53:01 +0100

    added routes for get post put and delete

--- back/routes/post.js
+++ back/routes/post.js
@@ -4,9 +4,9 @@ const multer = require("../middleware/multer-config");
 const auth = require("../middleware/auth");
 const postCtrl = require("../controllers/post");
 
-router.get("/", auth, postCtrl.getPosts);
-router.get("/:id", auth, postCtrl.getOnePost);
-router.post("/", auth, multer, postCtrl.createPost);
+router.get("/", auth, postCtrl.getAllPosts); //ADD BACK AUTH
+//router.get("/:id", auth, postCtrl.getOnePost);
+router.post("/", auth, multer, postCtrl.createPost); //ADD BACK AUTH AND MULTER
 router.put("/:id", auth, multer, postCtrl.modifyPost);
 router.delete("/:id", auth, postCtrl.deletePost);
 
