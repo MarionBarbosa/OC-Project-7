1cffdec2c637135ec9e55ca010fb5a70b7e22d5e
Marion L <mlelaumier@gmail.com>
2022-08-22 12:14:07 +0100

    Finished route PUT with file management

--- back/controllers/post.js
+++ back/controllers/post.js
@@ -1,10 +1,13 @@
 const Post = require("../models/post");
 const db = require("../mysql_config");
+const fs = require("fs");
+
+//CREATING NEW POST
 exports.createPost = (req, res, next) => {
+  //getting the body of the request
   const postObject = JSON.parse(req.body.post);
-  //sending new post to database
-  //checking userId is the same to authorise new post
-  if (postObject.userId != req.auth.userId) {
+  //checking if the userId is the same to authorise new post, if not matching throw err
+  if (postObject.userId !== req.auth.userId) {
     res.status(401).json({ message: "Accès non autorisé" });
   } else {
     const post = new Post({
@@ -13,6 +16,8 @@ exports.createPost = (req, res, next) => {
         req.file.filename
       }`,
     });
+
+    //sending new post into database
     db.query(
       "INSERT INTO post SET ?",
       [post.title, post.content, post.userId, post.imageUrl],
@@ -28,7 +33,7 @@ exports.createPost = (req, res, next) => {
     );
   }
 };
-
+//GETTING ALL POSTS
 exports.getAllPosts = (req, res, next) => {
   db.query("SELECT * FROM post", (error, results) => {
     if (error) {
@@ -36,60 +41,102 @@ exports.getAllPosts = (req, res, next) => {
     } else if (results == 0) {
       return res.status(404).json({ error: "Post non trouve" });
     } else {
-      console.log(results);
       res.status(200).json({ results });
     }
   });
 };
-exports.modifyPost = (req, res, next) => {
-  const newTitle = req.body.title;
-  const newContent = req.body.content;
-  const postId = req.body.id;
 
-  const post = new Post(newTitle, newContent);
-  db.query("SELECT * FROM post WHERE id=?", postId, (error, results) => {
+//MODIFYING POST
+exports.modifyPost = (req, res, next) => {
+  //getting body of the request
+  const postObject = req.file //case where the reauest contains a file
+    ? {
+        ...JSON.parse(req.body.post),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : //case where it does NOT contain a file
+      { ...JSON.parse(req.body.post) };
+  //Query to database to search for the post to be modified
+  db.query("SELECT * FROM post WHERE id=?", postObject.id, (error, results) => {
     if (error) {
       res.json({ error });
     } else if (results == 0) {
       return res.status(404).json({ error: "Post inexistant" });
+    } else if (postObject.userId !== req.auth.userId) {
+      return res.status(400).json({ message: "acces refuse" });
     } else {
-      db.query(
-        `UPDATE post SET ? WHERE id=?`,
-        [post, postId],
-        (error, results) => {
-          if (error) {
-            res.json({ error });
-          } else if (results == 0) {
-            return res.status(404).json({ error: "Post non modifie" });
-          } else {
-            res.status(200).json({ message: "post modifie" });
-          }
-        }
-      );
+      //getting he old image name in case it needs deleting
+      const oldFilename = results[0].imageUrl.split("/images/")[1];
+      req.file //case where the request contains a file
+        ? db.query(
+            `UPDATE post SET title=?, content=?, imageUrl=? WHERE id=?`,
+            [
+              postObject.title,
+              postObject.content,
+              postObject.imageUrl,
+              postObject.id,
+            ],
+            (error, results) => {
+              if (error) {
+                res.json({ error });
+              } else if (results == 0) {
+                return res.status(404).json({ error: "Post non modifie" });
+              } else {
+                //deleting previous image
+                fs.unlink(`images/${oldFilename}`, (err) => {
+                  message: "Image non supprimée";
+                });
+                res.status(200).json({ message: "post modifie" });
+              }
+            }
+          )
+        : //case where there is no file to be modified
+          db.query(
+            `UPDATE post SET title=?, content=? WHERE id=?`,
+            [postObject.title, postObject.content, postObject.id],
+            (error, results) => {
+              if (error) {
+                res.json({ error });
+              } else if (results == 0) {
+                return res.status(404).json({ error: "Post non modifie" });
+              } else {
+                res.status(200).json({ message: "post modifie" });
+              }
+            }
+          );
     }
   });
 };
+
+//DELETING POST
 exports.deletePost = (req, res, next) => {
-  const postId = req.body.id;
-  db.query("SELECT * FROM post WHERE id=?", postId, (error, results) => {
+  const post = req.body;
+  db.query("SELECT * FROM post WHERE id=?", post.id, (error, results) => {
     if (error) {
       res.json({ error });
     } else if (results == 0) {
       return res.status(404).json({ error: "Post inexistant" });
+    } else if (post.userId !== req.auth.userId) {
+      return res.status(400).json({ message: "acces refuse" });
     } else {
-      db.query("DELETE FROM post WHERE id =?", postId, (error, results) => {
-        if (error) {
-          res.json({ error });
-        } else if (results == 0) {
-          return res.status(404).json({ error: "Post non supprime" });
-        } else {
-          res.status(200).json({ message: "post supprime " });
-        }
+      //deleting image
+      const filename = post.imageUrl.split("/images/")[1];
+      fs.unlink(`images/${filename}`, () => {
+        db.query("DELETE FROM post WHERE id =?", post.id, (error, results) => {
+          if (error) {
+            res.json({ error });
+          } else if (results == 0) {
+            return res.status(404).json({ error: "Post non supprime" });
+          } else {
+            res.status(200).json({ message: "post supprime " });
+          }
+        });
       });
     }
   });
 };
-// const fs = require("fs");
 
 // exports.createPost = (req, res, next) => {
 //   const postObject = JSON.parse(req.body.post);
