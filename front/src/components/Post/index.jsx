32983854356a7be09e85042831dcf068f7df6c76
Marion L <mlelaumier@gmail.com>
2022-09-06 09:46:36 +0100

    added props

--- front/src/components/Post/index.jsx
+++ front/src/components/Post/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaRegThumbsUp, FaRegCommentAlt } from "react-icons/fa";
-export default function Post() {
+export default function Post(props) {
   const [showMenu, setShowMenu] = React.useState(false);
   function handleClick() {
     setShowMenu((prevShowMenu) => !prevShowMenu);
@@ -10,9 +10,20 @@ export default function Post() {
     setShowComments((prevShowComments) => !prevShowComments);
   }
   const [likeCount, setLikeCount] = React.useState(0);
-  function addLike() {
-    setLikeCount((prevLikeCount) => prevLikeCount + 1);
-  }
+  // function addLike() {
+  //   if (like === 1) {
+  //     setLikeCount((prevLikeCount) => prevLikeCount + 1);
+  //     //sending data to backend to save in database
+  //     //==>userID, like=1 ou -1, postId
+  //     fetch(url, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json;charset=UTF-8",
+  //       },
+  //       body: JSON.stringify(userId, like, postId),
+  //     });
+  //   }
+  // }
 
   return (
     <section className="post">
@@ -20,7 +31,7 @@ export default function Post() {
         <div className="post--profile--details">
           <div className="post--profile--picture">pic</div>
           <div className="post--profile--name">NAME</div>
-          <div>DATE</div>
+          <div>{props.timestamp}</div>
         </div>
         <div className="post--collapsibleMenu">
           <button onClick={handleClick}>+</button>
@@ -33,20 +44,13 @@ export default function Post() {
         </div>
       </div>
       <div>
-        <div className="post--image"></div>
-        <div className="post--text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+        <div className="post--title">{props.title}</div>
+        <div className="post--image">{props.imageUrl}</div>
+        <div className="post--text">{props.content}</div>
         <div className="post--interaction">
           <div className="post--interaction--button">
             <div className="post--like">
-              <button onClick={addLike}>
+              <button>
                 {likeCount} <FaRegThumbsUp />
               </button>
             </div>
