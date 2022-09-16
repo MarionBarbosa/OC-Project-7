7188bb4d7a05ae949a576d3aa39a477b48f98990
Marion L <mlelaumier@gmail.com>
2022-09-16 21:18:27 +0100

    created modal to be used on delete buttons

---
+++ front/src/components/ModalDelete/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function ModalDelete(props) {
+  function handleClickDelete(event) {
+    event.preventDefault();
+    console.log(event.currentTarget.id);
+    // const postId = event.currentTarget.id;
+    // const urlDeletePost =`http://localhost:3001/api/post/${postId}`;
+    //  fetch(urlDeletePost, {
+    //   method: "DELETE",
+    //   headers: { "Content-Type": "application/json;charset=UTF-8" },
+    //   body: JSON.parse({ postId }),
+    // });
+  }
+  return (
+    <div className="modal--background">
+      <div className="modal--container">
+        <h2>Confirmer la suppression</h2>
+
+        <div className="modal--button">
+          <button onClick={() => props.closeModalDelete(false)}>Annuler</button>
+          <button onClick={handleClickDelete} id={props.postId}>
+            Supprimer
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
