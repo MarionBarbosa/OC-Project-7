012144fca576bcbe96de1639ca164d6138f32c14
Marion L <mlelaumier@gmail.com>
2022-09-23 22:14:31 +0100

    added route to get all comment and modified name for comment per postId

--- back/routes/comment.js
+++ back/routes/comment.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const commentCtrl = require("../controllers/comment");
 
-router.get("/:postId", commentCtrl.getAllComment);
+router.get("/:postId", commentCtrl.getPostComment);
+router.get("/", commentCtrl.getAllComment);
 router.post("/", commentCtrl.createComment);
-router.put("/:postId/:commentId", commentCtrl.modifyComment);
-router.delete("/:postId/:commentId", commentCtrl.deleteComment);
+router.put("/:commentId", commentCtrl.modifyComment);
+router.delete("/:commentId", commentCtrl.deleteComment);
 
 module.exports = router;
