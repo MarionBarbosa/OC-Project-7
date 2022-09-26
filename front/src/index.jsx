1c9ba631c70fa93d021b81bca228edba43fc9073
Marion L <mlelaumier@gmail.com>
2022-09-26 10:53:56 +0100

    reorganise code

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./style.css";
-//import App from "./App";
 
-import Feed from "./pages/Feed";
-import Profile from "./pages/Profile";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import CreatePost from "./pages/CreatePost";
+import App from "./App";
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route exact path="/" element={<Feed />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signIn" element={<SignIn />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/createPost" element={<CreatePost />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
