fba6921b4a90fe4b604d338b399600478cb2a575
Marion L <mlelaumier@gmail.com>
2022-10-05 18:24:19 +0100

    magaed signin and localstorage

--- front/src/pages/SignIn/index.jsx
+++ front/src/pages/SignIn/index.jsx
@@ -1,25 +1,27 @@
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { UserContext } from "../../Context";
 
 export default function SignIn() {
-  const passwordRef = useRef();
-  const emailRef = useRef();
   let navigate = useNavigate();
-  const [errorEmail, setErrorEmail] = useState(null);
-  // const [loginData, setLoginData] = useState();
-  const { loginData, setLoginData } = useContext(UserContext);
-  console.log(loginData);
+  const [error, setError] = useState(null);
+  const { setIsLoggedIn } = useContext(UserContext);
+  const [formSignin, setFormSignin] = useState({ email: "", password: "" });
+
+  function handleChange(event) {
+    setFormSignin((prevFormSignin) => {
+      return {
+        ...prevFormSignin,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+  function handleClick() {
+    setError("");
+  }
   function handleSubmit(event) {
     event.preventDefault();
-    //setLoginData({ user: 1, token: "ooubjnjpuoyiuf", isAdmin: false });
-    const savedEmail = emailRef.current.value;
-    const savedPassword = passwordRef.current.value;
-    const savedData = {
-      email: savedEmail,
-      password: savedPassword,
-    };
     // **********TO DO ***********=>check if form is correctly filled and not empty before sending to DB
     const urlSignIn = "http://localhost:3001/api/auth/login";
     fetch(urlSignIn, {
@@ -27,35 +29,48 @@ export default function SignIn() {
       headers: {
         "Content-Type": "application/json;charset=UTF-8",
       },
-      body: JSON.stringify(savedData),
+      body: JSON.stringify(formSignin),
     })
-      .then(function(res) {
+      .then(function (res) {
+        console.log(res);
         if (res.ok) {
           return res.json();
-          // navigate("/", { replace: true });
         } else {
-          //ce n'est pas forcment a cause du mot de passe
-          setErrorEmail("Mot de passe errone");
+          setError("Mot de passe ou utilisateur incorrects");
         }
       })
       .then((data) => {
-        setLoginData(data);
-        console.log(data);
-        console.log(loginData);
-      })
-      //redirect to feed page
-      .then(() => {
-        navigate("/", { replace: true });
+        if (!data) {
+          setIsLoggedIn(false);
+        } else if (data.auth) {
+          setIsLoggedIn(true);
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("userId", data.userId);
+          localStorage.setItem("isAdmin", data.isAdmin);
+          navigate("/", { replace: true });
+        }
       });
   }
   return (
     <section className="signIn">
       <h1>Connectez-vous</h1>
       <form className="signIn__form">
-        <input type="email" placeholder="Email" ref={emailRef} />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+          onClick={handleClick}
+        />
 
-        <input type="password" placeholder="Mot de passe" ref={passwordRef} />
-        {errorEmail && <p style={{ color: "red" }}>{errorEmail}</p>}
+        <input
+          type="password"
+          placeholder="Mot de passe"
+          onChange={handleChange}
+          name="password"
+          onClick={handleClick}
+        />
+        {error && <p style={{ color: "red" }}>{error}</p>}
         <button className="signIn--button" onClick={handleSubmit}>
           Se connecter
         </button>
