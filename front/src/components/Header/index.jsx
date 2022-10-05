c206f0b6fd16d2fa5a6b4b34239887c459480cc9
Marion L <mlelaumier@gmail.com>
2022-10-05 18:21:44 +0100

    updated to manage logout for localstorage

--- front/src/components/Header/index.jsx
+++ front/src/components/Header/index.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo-black.png";
-import SearchBar from "../SearchBar";
+import Logo from "../../assets/icon-left-font-monochrome-white.png";
+import { FaUserCircle } from "react-icons/fa";
+import { UserContext } from "../../Context";
 //import { Link } from "react-router-dom";
 export default function Header() {
+  const { setIsLoggedIn } = useContext(UserContext);
+
+  function handleLogOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("isAdmin");
+    setIsLoggedIn(false);
+  }
   return (
     <header>
-      <Link to="/home">
-        <img src={Logo} alt="logo" className="header--logo" />
-      </Link>
-      <Link to="/createPost">Nouvelle publication</Link>
-      <SearchBar />
+      <div className="header--logo">
+        <Link to="/home">
+          <img src={Logo} alt="logo" className="header--logo--image" />
+        </Link>
+      </div>
+      <div className="header--newPost">
+        <Link to="/CreatePost" className="link">
+          <p>Nouvelle publication</p>
+        </Link>
+      </div>
       <section>
-        <div>pic</div>
-        <Link to="/profile">Profile</Link>
-        <Link to="/signIn">Deconnexion</Link>
+        <div>
+          <FaUserCircle />
+        </div>
+        <Link to="/profile" className="link">
+          Profile
+        </Link>
+        <Link to="/signIn" className="link" onClick={handleLogOut}>
+          Deconnexion
+        </Link>
       </section>
     </header>
   );
