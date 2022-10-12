224220ec853206213e362ca31be1c9505eb9d9f1
Marion L <mlelaumier@gmail.com>
2022-10-12 14:01:24 +0100

    deleted profile and added NotFound page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./style.css";
 
 import Feed from "./pages/Feed";
-import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import CreatePost from "./pages/CreatePost";
+import NotFound from "./pages/NotFound";
 import { UserContext } from "../src/Context/index";
 import { useState } from "react";
 import "./style.css";
@@ -20,10 +20,10 @@ export default function App() {
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<Feed />} />
-          <Route path="/profile" element={<Profile />} />
           <Route exact path="/signIn" element={<SignIn />} />
           <Route path="/signUp" element={<SignUp />} />
           <Route path="/createPost" element={<CreatePost />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </UserContext.Provider>
     </Router>
