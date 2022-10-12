499276bf440f79070028a5a4e5c12d5ba8fe5533
Marion L <mlelaumier@gmail.com>
2022-10-12 11:40:17 +0100

    Added regex to check if content is empty

--- back/controllers/post.js
+++ back/controllers/post.js
@@ -2,78 +2,90 @@ const db = require("../mysql_config");
 const fs = require("fs");
 
 let isAdmin = 0;
+function inputValidation(value) {
+  const isEmpty = /^(?!\s*$).+/;
+  if (!isEmpty.test(value)) {
+    return "champ vide";
+  } else {
+    return null;
+  }
+}
+
 //***************************CREATING NEW POST*********************************
 exports.createPost = (req, res) => {
   //getting the body of the request
   const postObject = req.body;
-
-  //checking if the userId is the same to authorise new post, if not matching throw err
-  if (+postObject.userId !== req.auth.userId) {
-    res.status(401).json({ message: "Access denied" });
-  } else if (!req.file) {
-    db.query(
-      "INSERT INTO post SET content=?, userId=?",
-      [postObject.content, postObject.userId],
-      (error, results) => {
-        if (error) {
-          res.json({ error });
-        } else if (results == 0) {
-          return res.status(404).json({ error: "Post not created" });
-        } else {
-          db.query(
-            "SELECT * FROM post WHERE id=?",
-            results.insertId,
-            (error, results) => {
-              if (error) {
-                res.json({ error });
-              } else if (results == 0) {
-                return res.status(404).json({ error: "post not found" });
-              } else {
-                res.status(200).json({
-                  results,
-                });
+  if (inputValidation(postObject.content) === null) {
+    //checking if the userId is the same to authorise new post, if not matching throw err
+    if (+postObject.userId !== req.auth.userId) {
+      res.status(401).json({ message: "Access denied" });
+    } else if (!req.file) {
+      db.query(
+        "INSERT INTO post SET content=?, userId=?",
+        [postObject.content, postObject.userId],
+        (error, results) => {
+          if (error) {
+            res.json({ error });
+          } else if (results == 0) {
+            return res.status(404).json({ error: "Post not created" });
+          } else {
+            db.query(
+              "SELECT * FROM post WHERE id=?",
+              results.insertId,
+              (error, results) => {
+                if (error) {
+                  res.json({ error });
+                } else if (results == 0) {
+                  return res.status(404).json({ error: "post not found" });
+                } else {
+                  res.status(200).json({
+                    results,
+                  });
+                }
               }
-            }
-          );
+            );
+          }
         }
-      }
-    );
-  } else if (req.file) {
-    const post = {
-      ...postObject,
-      imageUrl: `${req.protocol}://${req.get("host")}/images/${
-        req.file.filename
-      }`,
-    };
+      );
+    } else if (req.file) {
+      const post = {
+        ...postObject,
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      };
 
-    //sending new post into database
-    db.query(
-      "INSERT INTO post SET content=?, userId=?, imageUrl=?",
-      [post.content, post.userId, post.imageUrl],
-      (error, results) => {
-        if (error) {
-          res.json({ error });
-        } else if (results == 0) {
-          return res.status(404).json({ error: "Post not created" });
-        } else {
-          db.query(
-            "SELECT * FROM post WHERE id=?",
-            results.insertId,
-            (error, results) => {
-              if (error) {
-                res.json({ error });
-              } else if (results == 0) {
-                return res.status(404).json({ error: "post not found" });
-              } else {
-                res.status(200).json({
-                  results,
-                });
+      //sending new post into database
+      db.query(
+        "INSERT INTO post SET content=?, userId=?, imageUrl=?",
+        [post.content, post.userId, post.imageUrl],
+        (error, results) => {
+          if (error) {
+            res.json({ error });
+          } else if (results == 0) {
+            return res.status(404).json({ error: "Post not created" });
+          } else {
+            db.query(
+              "SELECT * FROM post WHERE id=?",
+              results.insertId,
+              (error, results) => {
+                if (error) {
+                  res.json({ error });
+                } else if (results == 0) {
+                  return res.status(404).json({ error: "post not found" });
+                } else {
+                  res.status(200).json({
+                    results,
+                  });
+                }
               }
-            }
-          );
+            );
+          }
         }
-      }
-    );
+      );
+    }
+  } else {
+    res.status(400).json({ message: "champ vide" });
   }
 };
 //********************************GETTING ALL POSTS************************************
@@ -93,102 +105,107 @@ exports.getAllPosts = (req, res, next) => {
 exports.modifyPost = (req, res, next) => {
   const postId = req.params.id;
   const postObject = req.body;
-  //checking if the logged user has admin rights
-  db.query(
-    "SELECT * FROM user WHERE id=?",
-    req.auth.userId,
-    (error, results) => {
+
+  if (inputValidation(postObject.content) === null) {
+    //checking if the logged user has admin rights
+    db.query(
+      "SELECT * FROM user WHERE id=?",
+      req.auth.userId,
+      (error, results) => {
+        if (error) {
+          res.json({ error });
+        } else if (results == 0) {
+          return res.status(404).json({ error: "user not found" });
+        } else if (results[0].admin === 1) {
+          isAdmin = 1;
+        } else {
+          isAdmin = 0;
+        }
+      }
+    );
+    //Query to database to search for the post to be modified
+    db.query("SELECT * FROM post WHERE id=?", postId, (error, results) => {
       if (error) {
         res.json({ error });
       } else if (results == 0) {
-        return res.status(404).json({ error: "user not found" });
-      } else if (results[0].admin === 1) {
-        isAdmin = 1;
+        return res.status(404).json({ error: "Post not found" });
+      } else if (isAdmin !== 1 && results[0].userId !== req.auth.userId) {
+        return res.status(400).json({ message: "access denied" });
       } else {
-        isAdmin = 0;
-      }
-    }
-  );
-  //Query to database to search for the post to be modified
-  db.query("SELECT * FROM post WHERE id=?", postId, (error, results) => {
-    if (error) {
-      res.json({ error });
-    } else if (results == 0) {
-      return res.status(404).json({ error: "Post not found" });
-    } else if (isAdmin !== 1 && results[0].userId !== req.auth.userId) {
-      return res.status(400).json({ message: "access denied" });
-    } else {
-      //getting the old image name in case it needs deleting
-      // const oldFilename = results[0].imageUrl.split("/images/")[1];
-      if (req.file) {
-        const postImage = {
-          ...postObject,
-          imageUrl: `${req.protocol}://${req.get("host")}/images/${
-            req.file.filename
-          }`,
-        };
+        //getting the old image name in case it needs deleting
+        // const oldFilename = results[0].imageUrl.split("/images/")[1];
+        if (req.file) {
+          const postImage = {
+            ...postObject,
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${
+              req.file.filename
+            }`,
+          };
 
-        db.query(
-          `UPDATE post SET content=?, imageUrl=? WHERE id=?`,
-          [postImage.content, postImage.imageUrl, postId],
-          (error, results) => {
-            if (error) {
-              res.json({ error });
-            } else if (results == 0) {
-              return res.status(404).json({ error: "Post not modified" });
-            } else {
-              //deleting previous image
-              // fs.unlink(`images/${oldFilename}`, (err) => {
-              //   message: "Image not deleted";
-              // });
-              db.query(
-                "SELECT * FROM post WHERE id=?",
-                postId,
-                (error, results) => {
-                  if (error) {
-                    res.json({ error });
-                  } else if (results == 0) {
-                    return res.status(404).json({ error: "Post not found" });
-                  } else {
-                    res.status(200).json({
-                      results,
-                    });
+          db.query(
+            `UPDATE post SET content=?, imageUrl=? WHERE id=?`,
+            [postImage.content, postImage.imageUrl, postId],
+            (error, results) => {
+              if (error) {
+                res.json({ error });
+              } else if (results == 0) {
+                return res.status(404).json({ error: "Post not modified" });
+              } else {
+                //deleting previous image
+                // fs.unlink(`images/${oldFilename}`, (err) => {
+                //   message: "Image not deleted";
+                // });
+                db.query(
+                  "SELECT * FROM post WHERE id=?",
+                  postId,
+                  (error, results) => {
+                    if (error) {
+                      res.json({ error });
+                    } else if (results == 0) {
+                      return res.status(404).json({ error: "Post not found" });
+                    } else {
+                      res.status(200).json({
+                        results,
+                      });
+                    }
                   }
-                }
-              );
+                );
+              }
             }
-          }
-        );
-      } else if (!req.file) {
-        const postObject = req.body;
-        db.query(
-          `UPDATE post SET content=? WHERE id=?`,
-          [postObject.content, postId],
-          (error, results) => {
-            if (error) {
-              res.json({ error });
-            } else if (results == 0) {
-              return res.status(404).json({ error: "Post not modified" });
-            } else {
-              db.query(
-                "SELECT * FROM post WHERE id=?",
-                postId,
-                (error, results) => {
-                  if (error) {
-                    res.json({ error });
-                  } else if (results == 0) {
-                    return res.status(404).json({ error: "Post not found" });
-                  } else {
-                    res.status(200).json({ results });
+          );
+        } else if (!req.file) {
+          const postObject = req.body;
+          db.query(
+            `UPDATE post SET content=? WHERE id=?`,
+            [postObject.content, postId],
+            (error, results) => {
+              if (error) {
+                res.json({ error });
+              } else if (results == 0) {
+                return res.status(404).json({ error: "Post not modified" });
+              } else {
+                db.query(
+                  "SELECT * FROM post WHERE id=?",
+                  postId,
+                  (error, results) => {
+                    if (error) {
+                      res.json({ error });
+                    } else if (results == 0) {
+                      return res.status(404).json({ error: "Post not found" });
+                    } else {
+                      res.status(200).json({ results });
+                    }
                   }
-                }
-              );
+                );
+              }
             }
-          }
-        );
+          );
+        }
       }
-    }
-  });
+    });
+  } else {
+    return res.status(400).json({ message: "champ vide" });
+  }
 };
 
 //****************************DELETING POST***********************************8
