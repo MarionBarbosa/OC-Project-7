e96343219ae7c255a28ff03c6c9ede685f01fb7c
Marion L <mlelaumier@gmail.com>
2022-10-12 10:14:24 +0100

    Added admin field

--- back/models/user.js
+++ back/models/user.js
@@ -1,14 +1,10 @@
-/*
- *creation d'un compte avec
- * => adresse email
- * => mot de passe
- */
 class User {
-  constructor(firstName, lastName, email, password) {
+  constructor(firstName, lastName, email, password, admin) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.password = password;
+    this.admin = admin;
   }
 }
 module.exports = User;
