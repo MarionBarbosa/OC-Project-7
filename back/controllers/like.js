87d993575073fea13bba45b48dcea086a7ff1c74
Marion L <mlelaumier@gmail.com>
2022-10-17 22:42:47 +0100

    destructured body

--- back/controllers/like.js
+++ back/controllers/like.js
@@ -2,10 +2,7 @@ const db = require("../mysql_config");
 //if like =1 check not already liked in database
 //if like=-1 delete like from database
 exports.createLike = (req, res, next) => {
-  let userId = req.body.userId;
-  let postId = req.body.postId;
-  let like = req.body.like;
-  console.log(req.body);
+  const { userId, postId, like } = req.body;
   //find if already liked
   db.query(
     "SELECT * FROM liked WHERE postId=? AND userId=?",
@@ -24,7 +21,7 @@ exports.createLike = (req, res, next) => {
             if (error) {
               res.json({ error });
             } else {
-              res.status(200).json({ message: "like added " });
+              res.status(200).json({ message: "like added" });
             }
           }
         );
