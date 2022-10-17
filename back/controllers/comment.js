3d8abf50bb8e6581a2fddd6b31c6765b9c5f233e
Marion L <mlelaumier@gmail.com>
2022-10-17 22:41:28 +0100

    managed empty field from front

--- back/controllers/comment.js
+++ back/controllers/comment.js
@@ -1,45 +1,56 @@
 const db = require("../mysql_config");
 let isAdmin = 0;
-
+function inputValidation(value) {
+  const isEmpty = /^(?!\s*$).+/;
+  if (!isEmpty.test(value)) {
+    return "champ vide";
+  } else {
+    return null;
+  }
+}
 // *******************CREATE COMMENT************************
 exports.createComment = (req, res, next) => {
   //getting all data for new post from frontend
   const commentObject = req.body;
-  if (commentObject.userId !== req.auth.userId) {
-    res.status(401).json({ message: "Access denied" });
-  } else {
-    const comment = {
-      ...commentObject,
-    };
-    //sending new comment into database
-    db.query(
-      "INSERT INTO comment SET content=?, userId=?, postId=?",
-      [comment.content, comment.userId, comment.postId],
-      (error, results) => {
-        if (error) {
-          res.json({ error });
-        } else if (results == 0) {
-          return res.status(404).json({ error: "Comment not saved" });
-        } else {
-          //getting the comment last inserted to send back to front
-          db.query(
-            "SELECT * FROM comment WHERE id=?",
-            results.insertId,
-            (error, results) => {
-              if (error) {
-                res.json({ error });
-              } else if (results == 0) {
-                return res.status(404).json({ error: "Comment not found" });
-              } else {
-                res.status(200).json({
-                  results,
-                });
+  if (inputValidation(commentObject.content) === null) {
+    if (commentObject.userId !== req.auth.userId) {
+      res.status(401).json({ message: "Access denied" });
+    } else {
+      const comment = {
+        ...commentObject,
+      };
+      //sending new comment into database
+      db.query(
+        "INSERT INTO comment SET content=?, userId=?, postId=?",
+        [comment.content, comment.userId, comment.postId],
+        (error, results) => {
+          if (error) {
+            res.json({ error });
+          } else if (results == 0) {
+            return res.status(404).json({ error: "Comment not saved" });
+          } else {
+            //getting the comment last inserted to send back to front
+            db.query(
+              "SELECT * FROM comment WHERE id=?",
+              results.insertId,
+              (error, results) => {
+                if (error) {
+                  res.json({ error });
+                } else if (results == 0) {
+                  return res.status(404).json({ error: "Comment not found" });
+                } else {
+                  res.status(200).json({
+                    results,
+                  });
+                }
               }
-            }
-          );
+            );
+          }
         }
-      }
-    );
+      );
+    }
+  } else {
+    res.status(400).json({ message: "champ vide" });
   }
 };
 
@@ -71,47 +82,56 @@ exports.getAllComment = (req, res, next) => {
 exports.modifyComment = (req, res, next) => {
   const comment = req.body;
   const commentId = req.params.commentId;
-
-  db.query(
-    "SELECT * FROM user WHERE id=?",
-    req.auth.userId,
-    (error, results) => {
-      if (error) {
-        res.json({ error });
-      } else if (results == 0) {
-        return res.status(404).json({ error: "user not found" });
-      } else if (results[0].admin === 1) {
-        isAdmin = 1;
-      } else {
-        isAdmin = 0;
+  if (inputValidation(comment.content) === null) {
+    db.query(
+      "SELECT * FROM user WHERE id=?",
+      req.auth.userId,
+      (error, results) => {
+        if (error) {
+          res.json({ error });
+        } else if (results == 0) {
+          return res.status(404).json({ error: "user not found" });
+        } else if (results[0].admin === 1) {
+          isAdmin = 1;
+        } else {
+          isAdmin = 0;
+        }
       }
-    }
-  );
-  db.query("SELECT * FROM comment WHERE id=?", commentId, (error, results) => {
-    if (error) {
-      res.json({ error });
-    } else if (results == 0) {
-      return res.status(404).json({ error: "Comment not found" });
-    } else {
-      if (isAdmin !== 1 && results[0].userId !== req.auth.userId) {
-        return res.status(400).json({ message: "access denied" });
-      } else {
-        db.query(
-          `UPDATE comment SET content=? WHERE id=?`,
-          [comment.content, commentId],
-          (error, results) => {
-            if (error) {
-              res.json({ error });
-            } else if (results == 0) {
-              return res.status(404).json({ error: "comment not modified" });
-            } else {
-              res.status(200).json({ message: "comment modified" });
-            }
+    );
+    db.query(
+      "SELECT * FROM comment WHERE id=?",
+      commentId,
+      (error, results) => {
+        if (error) {
+          res.json({ error });
+        } else if (results == 0) {
+          return res.status(404).json({ error: "Comment not found" });
+        } else {
+          if (isAdmin !== 1 && results[0].userId !== req.auth.userId) {
+            return res.status(400).json({ message: "access denied" });
+          } else {
+            db.query(
+              `UPDATE comment SET content=? WHERE id=?`,
+              [comment.content, commentId],
+              (error, results) => {
+                if (error) {
+                  res.json({ error });
+                } else if (results == 0) {
+                  return res
+                    .status(404)
+                    .json({ error: "comment not modified" });
+                } else {
+                  res.status(200).json({ message: "comment modified" });
+                }
+              }
+            );
           }
-        );
+        }
       }
-    }
-  });
+    );
+  } else {
+    res.status(400).json({ message: "champ vide" });
+  }
 };
 //*********************************DELETE COMMENT **************************************
 exports.deleteComment = (req, res, next) => {
