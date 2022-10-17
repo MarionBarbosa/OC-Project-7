18a6ea9c80bd3e34e495ef3f4f80e48d4dde9f23
Marion L <mlelaumier@gmail.com>
2022-10-17 22:45:41 +0100

    added getOneUser route

--- back/routes/user.js
+++ back/routes/user.js
@@ -5,5 +5,5 @@ const userCtrl = require("../controllers/user");
 
 router.post("/signUp", userCtrl.signup);
 router.post("/login", userCtrl.login);
-
+router.get("/:userId", userCtrl.getOneUser);
 module.exports = router;
