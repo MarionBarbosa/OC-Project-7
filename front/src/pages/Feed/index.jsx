e34ecbb5cdbeb86bc531eb56d81799c84acbc55b
Marion L <mlelaumier@gmail.com>
2022-10-21 22:10:23 +0100

    changed html, context error 401

--- front/src/pages/Feed/index.jsx
+++ front/src/pages/Feed/index.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
-import Header from "../../components/Header";
-
+import React, { useState, useEffect, useContext } from "react";
+import Navbar from "../../components/Nav";
+import { UserContext } from "../../Context";
 import Post from "../../components/Post";
-
+import { useNavigate } from "react-router-dom";
 export default function Feed() {
+  let navigate = useNavigate();
   const [postData, setPostData] = useState([]);
   const [commentData, setCommentData] = useState([]);
-
+  const { isAuthenticated, setIsAuthenticated } = useContext(UserContext);
   //function to update the state getting all the comments after adding a new comment so it is rendered on the page.
   const updateComment = (newComment) => {
     setCommentData((prevCommentData) => [...prevCommentData, newComment]);
@@ -15,7 +16,6 @@ export default function Feed() {
   const token = localStorage.getItem("token");
   //getting all posts
   useEffect(() => {
-    //setLoading(true);
     fetch("http://localhost:3001/api/post", {
       method: "GET",
       headers: {
@@ -54,6 +54,8 @@ export default function Feed() {
               setCommentData(getCommentData.results);
             })
             .catch((error) => console.error("error:", error));
+        } else if (res.status === 401) {
+          setIsAuthenticated(false);
         }
       })
       .catch((error) => console.error("error:", error));
@@ -77,10 +79,16 @@ export default function Feed() {
       />
     );
   });
-
+  useEffect(() => {
+    console.log("useEffect feed");
+    if (!isAuthenticated) {
+      navigate("/signIn", { replace: true });
+    }
+  }, []);
   return (
     <>
-      <Header />
+      <Navbar />
+
       <div className="feed">{postElements}</div>
     </>
   );
